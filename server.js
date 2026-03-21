b760b3b4de70356b1e84b710d32d6a54cc70164e
Jamron10 <narekmxeyan2025@gmail.com>
2026-03-21 22:51:55 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const TelegramBot = require('node-telegram-bot-api');
 
 // ==========================================
